e323429c5275d414fdab8fa70986b6b6f15c990c
yasharthdixit0107 <yasharthdixit0107@gmail.com>
2025-10-24 01:36:58 +0530

    Iniatial commit

---
+++ index.js
@@ -0,0 +1,295 @@
+console.log('radhe radhe sabhi ko');
+
+let a = 'yasharth';  /* use of colons is mandatory in java script  */
+console.log(a);
+
+
+/* let let = '6';
+console.log(let);
+                       this is not allowed or the keyword cant be a  reserved keyword 
+                       otherwise it will shows the error of lexically bounded */
+
+                       let lastName = 'yasharth';
+
+                       console.log(lastName);
+
+                       lastName = 7;
+                       console.log(lastName);
+
+
+                       let names = ['yash', 'mukul'];
+                     console.log(names[1]);
+                     console.log(names[2] = 'yogesh');
+
+                     let s= 23;
+                     let y = 34;
+
+                     console.log(s+y);
+
+
+                     console.log(2>3);
+                     console.log(5 != 3); /* !== :-  is sign ka means hai not equal to  */
+
+
+                     let age = 1;
+                     let voteStatus = (age >= 18 ) ? 'yes you can vote' : 'Pehle Bada Hoja';
+
+                     console.log(voteStatus);
+
+
+                    /* IF-ELSE-ELSEIF STATEMENT */
+                     let marks = 68;
+                     if(marks >= 75) {
+                        console.log('A');
+
+                     }
+                     else if(marks >= 68) {
+                        console.log('B');
+                     }
+
+                     else if (marks >= 50) {
+                        console.log('c');
+                     }
+
+                     else if (marks >= 40) {
+                        console.log(D);
+                     }
+
+                     else {
+                        console.log('FAIL');
+                     }
+
+
+                     /* SWITCH CASE  */
+
+                     let num = 0;
+
+                     switch(num) {
+                        case 1: console.log('A');
+                        break;
+                        case 2: console.log('B');
+                        break;
+                        case 3: console.log('C');
+                        break;
+                        default: console.log('D');
+                     }
+
+                     /* LOOPS */
+
+                     for(let i = 0; i < 9; i=i+1){
+                        console.log("YASHARTH" + i);
+                     }
+
+                     /* WHILE LOOP  */
+                     let i = 0;
+                     while (i<10)
+                     {
+                        console.log(i);
+
+                        i++;
+                     }
+
+                     /* DO-WHILE LOOP */
+
+                     let q = 11;
+
+                     do {
+                        console.log(q);
+                        q++;
+                     }while(q < 18);
+                     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
